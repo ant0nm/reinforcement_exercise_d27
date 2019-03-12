fd33ab5fd1913a1a15925a73e2072ba30d0a8bfb
Anton <anton.mois33v@gmail.com>
2019-03-12 10:03:46 -0400

    Exercise complete

---
+++ exercise.py
@@ -0,0 +1,17 @@
+e = [
+{'id': "38fj8d900", 'city': 'Hamilton', 'events': [{'date': '2017-01-01', 'attendees': 100}, {'date': '2016-12-31', 'attendees': 60}]},
+{'id': "39fo837y7", 'city': 'Toronto', 'events': [{'date': '2017-03-30', 'attendees': 3000}, {'date': '2017-07-07', 'attendees': 2500}, {'date': '2017-02-04', 'attendees': 900}]},
+{'id': "58uj8d800", 'city': 'Montreal', 'events': [{'date': '2017-08-10', 'attendees': 250}]},
+{'id': "48hn8d900", 'city': 'Kingston', 'events': [{ 'date': '2015-04-16', 'attendees': 45}]}
+]
+
+def display_events(data):
+    for dict in data:
+        print(dict['city'])
+        print("------------")
+        for event in dict["events"]:
+            print("Date: {}, {} people".format(event['date'], event['attendees']))
+        if data.index(dict) + 1 != len(data):
+            print()
+
+display_events(e)
